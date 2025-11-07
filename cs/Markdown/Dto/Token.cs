cd9e0f4b50c01cec30205c827e5398885aa670e1
younggogy <younggogy@gmail.com>
2025-11-07 22:11:11 +0500

    Вынес новые enum для удобной работы с токенами. Сделал Token.cs и Tag.cs не record классами с логикой. Благодаря логике в Tag.cs рендер работает быыстрее, т.к. не нужно делать долгие проверки на decimal.tryparse и добавление контента

--- cs/Markdown/Dto/Token.cs
+++ cs/Markdown/Dto/Token.cs
@@ -1,3 +1,49 @@
 namespace Markdown.Dto;
 
-public record Token(string Value, TokenType Type, bool CanOpen, bool CanClose, bool InsideWord);
+public class Token
+{
+    public string Value { get; private set; }
+    public TokenType Type { get; private set; }
+    public TokenRole Role { get; private set; }
+    public TokenPosition Position { get; private set; }
+
+    public Token(string value, TokenType type, TokenRole role, TokenPosition position)
+    {
+        Value = value;
+        Type = type;
+        Role = role;
+        Position = position;
+    }
+
+    public Token(string value, TokenType type, bool canOpen, bool canClose)
+    {
+        Value = value;
+        Type = type;
+        DetermineRole(canOpen, canClose);
+        DeterminePosition(canOpen, canClose);
+    }
+    
+    
+    private void DetermineRole(bool canOpen, bool canClose)
+    {
+        Role = (canOpen, canClose) switch
+        {
+            (true, true) => TokenRole.Both,
+            (true, false) => TokenRole.Open,
+            (false, true) => TokenRole.Close,
+            (false, false) => TokenRole.None
+        };
+    }
+
+    private void DeterminePosition(bool canOpen, bool canClose)
+    {
+        Position = (canOpen, canClose) switch
+        {
+            (true, true) => TokenPosition.Inside,
+            (true, false) => TokenPosition.Begin,
+            (false, true) => TokenPosition.End,
+            (false, false) => TokenPosition.None
+        };
+    }
+    
+}
